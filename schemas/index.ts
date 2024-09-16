b40eb74b215297345e14e458080136083682ba57
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-16 17:03:03 +0300

    register

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,14 @@ export const loginSchema = z.object({
     message: "Password is Required",
   }),
 });
+export const registerSchema = z.object({
+  email: z.string().email({
+    message: "Email is Required",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
+  name: z.string().min(1, {
+    message: "Name is Required",
+  }),
+});

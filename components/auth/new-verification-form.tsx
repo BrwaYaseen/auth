7f1c8f547189401e77b4ffb2838df5a5361caa01
Brwa Yaseen <brwayasin29@gmail.com>
2024-09-18 21:19:31 +0300

    email verified

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { CardWrapper } from "./card-wrapper";
+import { BeatLoader } from "react-spinners";
+import { useCallback, useEffect, useState } from "react";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+
+export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (success || error) return;
+    if (!token) {
+      setError("Missing Token");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      })
+      .catch(() => {
+        setError("Something went wrong");
+      });
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirmin your verification"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+    >
+      <div className="flex w-full items-center justify-center">
+        {!error && !success && <BeatLoader />}
+        <FormSuccess message={success} />
+        {!success && <FormError message={error} />}
+      </div>
+    </CardWrapper>
+  );
+};
